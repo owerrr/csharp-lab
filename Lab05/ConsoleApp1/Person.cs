f9b2f20a8cf1adcdfb8e088616a2c76aed4575be
owr <proroog@gmail.com>
2024-11-26 16:58:13 +0100

    Lab05
    [+] Lab05

---
+++ Lab05/ConsoleApp1/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // class declaration
+    public class Person
+    {
+        // constructor
+        public Person() { }
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = -1;
+        }
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthDate = birthDate;
+            SetAge();
+        }
+
+        // properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; private set; }
+        public string EmailAddress { get; private set; }
+        private int Age { get; set; }
+
+        // method
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        private void SetAge()
+        {
+            this.Age = (int)(DateTime.Now - this.BirthDate).TotalDays / 365;
+        }
+
+        public int GetAge()
+        {
+            if (this.Age == -1)
+                SetAge();
+
+            return this.Age;
+        }
+
+        public bool IsAdult()
+        {
+            if(this.Age == -1)
+                SetAge();
+
+            return this.Age >= 18;
+        }
+    }
+}
+
+
---
+++ Lab05/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static double CountCoordsLength(Coords[] c)
+        {
+            double res = 0.0;
+            for (int i = 0; i < c.Length-1; i++)
+            {
+                res += Math.Sqrt(Math.Pow(c[i].X - c[i + 1].X, 2) + Math.Pow(c[i].Y - c[i + 1].Y, 2));
+            }
+            return res;
+        }
+
+        public static int[] getMinAndMax(int[] input) {
+            int[] res = { input[0], input[0] };
+                for(int i = 1; i < input.Length; i++)
+                {
+                    if(res[0] > input[i]) res[0] = input[i];
+                    else if(res[1] < input[i]) res[1] = input[i];
+            }
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            Coords point1 = new Coords(1.0, 1.0);
+            Console.WriteLine(point1);
+
+            Coords point2 = new Coords(1.0, 1.0); ;
+
+            if (point1.Equals(point2))
+                Console.WriteLine("Equal structs");
+            else
+                Console.WriteLine("Different structs");
+
+            Console.WriteLine("\n=========================\n\tZADANIE 1\n=========================");
+            point1 = new Coords(1.0, 1.0);
+            point2 = new Coords(2.0, 2.0);
+            Console.WriteLine($"Line start=({point1.X},{point1.Y})  Line end=({point2.X},{point2.Y})");
+            Coords[] coords = {point1, point2 };
+            Console.WriteLine(CountCoordsLength(coords));
+
+            Console.WriteLine("\n=========================\n\tZADANIE 2\n=========================");
+            int[] minAndMaxInput = { 2, 3, 16, 2, -25, 2, 0, 1, 4 };
+            int[] filtered = getMinAndMax(minAndMaxInput);
+            Console.WriteLine($"Min = {filtered[0]}\nMax = {filtered[1]}");
+
+            Console.WriteLine("\n=========================\n\tZADANIE 3\n=========================");
+            Person person1 = new Person();
+            person1.FirstName = "John";
+            person1.LastName = "Doe";
+            Console.WriteLine($"Person={person1.FirstName} {person1.LastName}");
+            //Console.WriteLine(DateTime.Today.ToString());
+
+            
+            Person person2 = new Person("Andrzej", "Kmicic", new DateTime(2001, 06, 22));
+            string isAdult = person2.IsAdult() ? $"jestes pelnoletni!\nmasz juz {person2.GetAge()} lat!" : $"nie jestes pelnoletni!\nmasz dopiero {person2.GetAge()} lat!";
+            Console.WriteLine($"Hej {person2.FirstName} {person2.LastName}!\nWidze, ze {isAdult}");
+        }
+    }
+}
